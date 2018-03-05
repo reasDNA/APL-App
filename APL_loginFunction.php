075f0c10b97e6c28a9afaddfc262e5406d932d3c
martinpsve <martin-1@live.se>
2018-03-05 09:44:23 +0100

    Add files via upload

---
+++ APL_loginFunction.php
@@ -0,0 +1,12 @@
+<?php
+include 'APL_databasConnect.php';
+
+
+
+
+
+
+
+
+
+?>
---
+++ APL_startsida.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+ <!-- Inmatning av användarens id-nummer -->
+
+<div>
+
+<form action="APL_SQL-Frågor.php" method="get">
+
+
+<input type="submit" name='Elever+Handledare' value='Elever+Handledare'>
+
+</div>
+</form>
+</body>
+</html>
