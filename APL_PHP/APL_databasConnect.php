ce365a2474484e24f401a7d4b7814c34cba4c7c1
martinpsve <martin-1@live.se>
2018-03-05 09:20:09 +0100

    Delete APL_databasConnect.php

--- APL_PHP/APL_databasConnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Anslutning till databasen
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "APL";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
